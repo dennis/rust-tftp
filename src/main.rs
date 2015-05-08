f64be8048ce746085c7040ad0daf5da1446f4fa1
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2015-05-09 01:04:33 +0200

    Refactored - keeping it DRY

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::UdpSocket;
+use std::iter::Iterator;
 
 const MAX_PACKET_SIZE : usize = 512;
 
@@ -21,46 +22,44 @@ fn main() {
     }
 }
 
-fn decode(p : &[u8]) {
+fn decode(p : & [u8]) {
     let mut iter = p.iter();
-    let mut filename = "".to_string();
-    let mut mode_name = "".to_string();
 
-    // opcode
-    let opcode = decode_u16(*iter.next().unwrap(), *iter.next().unwrap());
-    println!("opcode: {}", opcode);
+    let opcode    = decode_u16(&mut iter).unwrap();
+    let filename  = decode_string(&mut iter).unwrap();
+    let mode_name = decode_string(&mut iter).unwrap();
 
-    // filename
-    while let Some(x) = iter.next() {
-        if *x == 0 {
-            break;
-        }
-        else {
-            let ch = std::char::from_u32(*x as u32).unwrap();
-            filename.push(ch);
-        }
-    }
     println!("filename: {}", filename);
-
-    // mode
-    while let Some(x) = iter.next() {
-        if *x == 0 {
-            break;
-        }
-        else {
-            let ch = std::char::from_u32(*x as u32).unwrap();
-            mode_name.push(ch);
-        }
-    }
+    println!("opcode: {}", opcode);
     println!("mode_name: {}", mode_name);
 }
 
-fn decode_u16(high : u8, lower : u8) -> u16 {
-    let mut result : u16 = high as u16;
+fn decode_u16<'a, I : Iterator<Item=&'a u8>>(iter : &mut I) -> Option<u16> {
+    if let Some(i) = iter.next() {
+        let mut r = *i as u16;
+        r = r << 8;
 
-    result = result << 8;
-    result = result + (lower as u16);
+        if let Some(j) = iter.next() {
+            r = r + *j as u16;
+            return Some(r)
+        }
+    }
 
-    result
+    None
 }
 
+fn decode_string<'a, I : Iterator<Item=&'a u8>>(iter : &mut I) -> Option<String> {
+    let mut string = "".to_string();
+
+    while let Some(x) = iter.next() {
+        if *x == 0 {
+            return Some(string)
+        }
+        else {
+            let ch = std::char::from_u32(*x as u32).unwrap();
+            string.push(ch);
+        }
+    }
+
+    None
+}
