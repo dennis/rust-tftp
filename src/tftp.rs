a259dfb3ca70e8929bc6a4681a538b136b2aac10
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2015-05-31 13:46:58 +0200

    Move all packet sending to own function

--- src/tftp.rs
+++ src/tftp.rs
@@ -21,6 +21,15 @@ struct Session {
     buffer : Vec<u8>,
 }
 
+fn send_packet(socket : &UdpSocket, peer : &SocketAddr, packet : Packet) {
+    let out = Protocol::encode(packet).unwrap();
+    println!("Sending {} bytes", out.len());
+
+    // if we cannot send it, we just silently ignore it. The session will
+    // eventually get expired anyway
+    socket.send_to(&out[..], peer).unwrap();
+}
+
 pub fn wip_server(local_addr : &str) {
     let socket = match UdpSocket::bind(local_addr) {
         Ok(s) => s,
@@ -60,9 +69,7 @@ pub fn wip_server(local_addr : &str) {
                     Vacant(_) => {
                         // We have just added it, so this shouldn't be possible
                         // No session found
-                        let out = Protocol::encode(Packet::ERROR(ErrorCode::UnknownTransferId, "".to_string())).unwrap();
-                        println!("Sending {} bytes", out.len());
-                        socket.send_to(&out[..], src).unwrap();
+                        send_packet(&socket, &src, Packet::ERROR(ErrorCode::UnknownTransferId, "".to_string()));
                     },
                     Occupied(entry) => {
                         let mut session = entry.into_mut();
@@ -120,50 +127,45 @@ pub fn wip_server(local_addr : &str) {
 fn handle_rrq(session : &mut Session, socket : &UdpSocket, src : &SocketAddr, filename : String, mode_name : String) {
     println!("RRQ opcode=1, filename={}, mode_name={}", filename, mode_name);
 
-    let out;
     match handle_file_read(filename) {
         Ok(stream) => {
             session.send_stream = stream;
-            out = send_data_block(session, 1);
+            send_data_block(session, &socket, &src, 1);
         },
         Err(_) => {
             // File not found
-            out = Protocol::encode(Packet::ERROR(ErrorCode::FileNotFound, "Test".to_string())).unwrap()
+            send_packet(&socket, &src, Packet::ERROR(ErrorCode::FileNotFound, "Test".to_string()))
         }
     }
-    println!("  Sending {} bytes", out.len());
-    socket.send_to(&out[..], src).unwrap();
 }
 
-fn send_data_block(session : &mut Session, block_no : u16) -> Vec<u8> {
+fn send_data_block(session : &mut Session, socket : &UdpSocket, src : &SocketAddr, block_no : u16) {
     let start : usize = ((block_no - 1) as usize) * MAX_PACKET_SIZE;
     let length : usize = MAX_PACKET_SIZE;
-    println!("  send data block: start={}, length={}", start, length);
+
+    println!("  Send data block: start={}, length={}", start, length);
+
     if let Ok(bytes) = session.send_stream.get_block(start, length) {
         session.buffer = bytes.clone();
         session.last_sent_block_no = block_no;
-        println!("  Sending Data block {}, {} bytes", block_no, bytes.len());
-        Protocol::encode(Packet::Data(block_no, &bytes)).unwrap()
+        send_packet(&socket, &src, Packet::Data(block_no, &bytes));
     }
     else {
-        Protocol::encode(Packet::ERROR(ErrorCode::NotDefined, "I/O error eading block".to_string())).unwrap()
+        send_packet(&socket, &src, Packet::ERROR(ErrorCode::NotDefined, "I/O error eading block".to_string()))
     }
 }
 
 fn handle_ack(session : &mut Session, socket : &UdpSocket, src : &SocketAddr, block_no : u16) {
     println!("ACK opcode=4, block_no={}, expected={}", block_no, session.last_sent_block_no);
 
-    let out;
-    // we might send out block that does not exists
+    // FIXME we might send out block that does not exists
     if block_no == session.last_sent_block_no {
         println!("  Sending next block {}", block_no + 1);
-        out = send_data_block(session, block_no + 1);
+        send_data_block(session, &socket, &src, block_no + 1);
     }
     else {
-        out = Protocol::encode(Packet::ERROR(ErrorCode::UnknownTransferId, format!("expected={}, got={}", block_no, session.last_sent_block_no))).unwrap();
+        send_packet(socket, src, Packet::ERROR(ErrorCode::UnknownTransferId, format!("expected={}, got={}", block_no, session.last_sent_block_no)));
     }
-    println!("  Sending {} bytes", out.len());
-    socket.send_to(&out[..], src).unwrap();
 }
 
 fn handle_file_read(filename : String) -> Result<Box<TftpReadStream>, ()> {
